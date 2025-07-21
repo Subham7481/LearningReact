c8aed02ce8f7f3a4bae33ff75d056df682f79e67
Subham Kaumar <patelsk7481@gmail.com>
2025-07-21 21:56:10 +0530

    Learned hooks in React.

--- ReactBasics/01basicreact/src/App.jsx
+++ ReactBasics/01basicreact/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Chai from './chai'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello World</h1>
-    <div className='flex justify-center items-center h-screen bg-gray-100'>
-      <button className='px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700'>
-        Click me!
-      </button>
-    </div>
+      <Chai/>
+      <h1>Hello Subham</h1>
     </>
   )
 }
---
+++ ReactBasics/02counterproject/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(5)
+
+  const addValue = () => {
+    console.log('Clicked!', counter)
+    
+    if(counter < 20){
+      counter += 1
+      setCounter(counter)
+    }
+  }
+
+  const removeValue = () => {
+    console.log('Removed', counter)
+     
+    if(counter > 0){
+      counter -= 1
+    setCounter(counter)
+    }
+  }
+
+  return (
+    <>
+    <h1>Chai Aur React</h1>
+    <h2>Counter Value : {counter}</h2>
+    <button onClick={addValue}>Increase value</button>
+    <br />
+    <button onClick={removeValue}>Decrease value</button>
+    </>
+  )
+}
+
+export default App
