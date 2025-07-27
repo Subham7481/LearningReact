1ae0a008baba20f4ff321640ac8039a4f7337e32
Subham Kaumar <patelsk7481@gmail.com>
2025-07-27 12:54:13 +0530

    Learned about hooks more

--- ReactBasics/04practicereact/src/App.jsx
+++ ReactBasics/04practicereact/src/App.jsx
@@ -2,32 +2,45 @@ import { useEffect ,useState } from 'react'
 import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [task, setTask] = useState([]);
+  const [inputvalue, setValue] = useState('');
 
-  useEffect(() => {
-    //API Call
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => setUsers(data))
-    .catch(error => console.log('Error fetching data :', error));
+  const handleInputChange = (e) => {
+    setValue(e.target.value)
+  }
 
-  }, []);
+  const addTask = () =>{
+    if(inputvalue.trim() !== ''){
+      setTask([...task, inputvalue]);
+      setValue('')
+    }
+  }
 
-  return (
-    <div>
-      <h2>Users List</h2>
-      <ul>
-        {users.map(user => (<li key={user.id}>{user.name}</li>))}
+ return (
+    <div className="p-4">
+      <h2 className="text-white text-xl mb-4">Your Tasks:</h2>
+      <ul className="text-white mb-4">
+        {tasks.map((task, index) => (
+          <li key={index} className="mb-1">- {task}</li>
+        ))}
       </ul>
 
-      <div>
-        <ul style={{display: "flex", listStyle: "none" }}>
-        <li>Milk</li>
-        <li>Egg</li>
-        <li>Flour</li>
-        <li>Tea</li>
-      </ul>
+      <div className="flex gap-2 items-center mb-2">
+        <label htmlFor="text" className="text-white">Enter the task:</label>
+        <input 
+          type="text"
+          className="bg-black text-white px-4 py-2 rounded-2xl"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
       </div>
+
+      <button 
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={addTask}
+      >
+        Add task
+      </button>
     </div>
   );
 }
